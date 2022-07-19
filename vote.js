b104077a1a37396ba444fa8f0f637ff74f7afc41
mokgothu <refilwemonoketsi@gmail.com>
2022-07-19 15:19:26 +0200

    first commit

---
+++ vote.js
@@ -0,0 +1,17 @@
+var vote;
+
+function DisplayVote(){
+ document.getElementById("DisplayVote"),innerHTML=vote;
+}
+
+  function age(){
+    if(age.value>18){
+    age=document.getElementById("out").innerHTML="You can vote";
+     return age;
+
+    }
+
+    else 
+        age=document.getElementById("out").innerHTML="You can't vote";
+      
+    }
